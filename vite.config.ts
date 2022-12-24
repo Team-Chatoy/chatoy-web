e936bf122bbe4add2554819e96c4ac2f31e97587
yurzhang <yurzhang@163.com>
2022-12-24 15:59:50 +0800

    CI: fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
+  base: "/chatoy-web/",
   plugins: [solidPlugin()],
   server: {
     port: 3000,
